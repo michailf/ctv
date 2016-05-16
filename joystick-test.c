85ff8b25ad65a343ef6daca88afb4b80d35ce06d
Serge Voilokov <serge0x76@gmail.com>
2016-05-15 22:31:40 -0400

    Tests with dbus, joystick and omxplayer

--- joystick-test.c
+++ joystick-test.c
@@ -17,9 +17,11 @@ static char *keystr[1024] = {
 	[KEY_HOME] = "HOME"
 };
 
+int player_started = 0;
+
 int main()
 {
-	int count = 0;
+	int count = 0, rc;
 
 	log_set(stderr);
 	joystick_init();
@@ -35,6 +37,21 @@ int main()
 			logi("%d ch: %3d %s\r", count++, ch, keystr[ch]);
 		if (ch == 13)
 			logi("=====================================\r");
+			
+		if (ch == KEY_LEFT && player_started == 0) {
+			ch = 'q';
+		} else if (ch == KEY_LEFT && player_started == 1) {
+			rc = system("~/src/ctv/dbuscontrol.sh stop");
+			logi("dbus.stop. rc: %d\r", rc);
+			player_started = 0;
+		} else if (ch == KEY_RIGHT && player_started == 0) {
+			rc = system("omxplayer --win 100,100,640,480 -s ~/b/ctvb/1.mp4 >/dev/null &");
+			logi("start omxplayer. rc: %d\r", rc);
+			player_started = 1;
+		} else if (ch == KEY_RIGHT && player_started == 1) {
+			rc = system("~/src/ctv/dbuscontrol.sh seek 60000000");
+			logi("dbus.seek. rc: %d\r", rc);
+		}
 	}
 
 	endwin();
